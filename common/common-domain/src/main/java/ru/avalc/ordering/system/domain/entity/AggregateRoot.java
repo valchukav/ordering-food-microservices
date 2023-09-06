bfa128876a7193a447cb2adb0e5a58626d080a96
Alexei Valchuk <a.valchukav@gmail.com>
2023-09-06 17:21:44 +0300

    chore: provide Common module

---
+++ common/common-domain/src/main/java/ru/avalc/ordering/system/domain/entity/AggregateRoot.java
@@ -0,0 +1,9 @@
+package ru.avalc.ordering.system.domain.entity;
+
+/**
+ * @author Alexei Valchuk, 06.09.2023, email: a.valchukav@gmail.com
+ */
+
+public abstract class AggregateRoot<ID> extends BaseEntity<ID>{
+
+}
---
+++ common/common-domain/src/main/java/ru/avalc/ordering/system/domain/entity/BaseEntity.java
@@ -0,0 +1,33 @@
+package ru.avalc.ordering.system.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+/**
+ * @author Alexei Valchuk, 06.09.2023, email: a.valchukav@gmail.com
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BaseEntity<ID> {
+
+    private ID id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseEntity<?> that)) return false;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
