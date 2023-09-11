ace27aa95f7863839d6ad57a74928a9dd37aac24
Alexei Valchuk <a.valchukav@gmail.com>
2023-09-11 09:57:18 +0400

    feat: provide Kafka consumer

---
+++ infrastructure/kafka/kafka-consumer/src/main/java/ru/avalc/ordering/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,14 @@
+package ru.avalc.ordering.kafka.consumer;
+
+import org.apache.avro.specific.SpecificRecordBase;
+
+import java.util.List;
+
+/**
+ * @author Alexei Valchuk, 11.09.2023, email: a.valchukav@gmail.com
+ */
+
+public interface KafkaConsumer<T extends SpecificRecordBase> {
+
+    void receive(List<T> messages, List<Long> keys, List<Integer> partitions, List<Long> offsets);
+}
