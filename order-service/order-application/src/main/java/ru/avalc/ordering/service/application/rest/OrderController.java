09ceac30319536953df2f10f4a3a532c2db238ab
Alexei Valchuk <a.valchukav@gmail.com>
2023-09-11 12:57:21 +0400

    feat: provide rest controller for Order service

---
+++ order-service/order-application/src/main/java/ru/avalc/ordering/service/application/rest/OrderController.java
@@ -0,0 +1,48 @@
+package ru.avalc.ordering.service.application.rest;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.avalc.ordering.service.domain.dto.create.CreateOrderCommand;
+import ru.avalc.ordering.service.domain.dto.create.CreateOrderResponse;
+import ru.avalc.ordering.service.domain.dto.track.TrackOrderQuery;
+import ru.avalc.ordering.service.domain.dto.track.TrackOrderResponse;
+import ru.avalc.ordering.service.domain.ports.input.service.OrderApplicationService;
+
+import java.util.UUID;
+
+/**
+ * @author Alexei Valchuk, 11.09.2023, email: a.valchukav@gmail.com
+ */
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/orders", produces = "application/vnd.api.v1+json")
+@AllArgsConstructor
+public class OrderController {
+
+    private final OrderApplicationService orderApplicationService;
+
+    @PostMapping
+    public ResponseEntity<CreateOrderResponse> createOrder(@RequestBody CreateOrderCommand createOrderCommand) {
+        log.info("Creating order for Customer: {} at Restaurant: {}",
+                createOrderCommand.getCustomerID(), createOrderCommand.getRestaurantID());
+
+        CreateOrderResponse orderResponse = orderApplicationService.createOrder(createOrderCommand);
+        log.info("Order created with tracking id: {}", orderResponse.getOrderTrackingID());
+
+        return new ResponseEntity<>(orderResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TrackOrderResponse> getOrderByTrackingID(@PathVariable("id") UUID trackingID) {
+        TrackOrderResponse trackOrderResponse = orderApplicationService.trackOrder(TrackOrderQuery.builder()
+                .orderTrackingID(trackingID)
+                .build());
+        log.info("Returning order status with tracking id: {}", trackOrderResponse.getOrderTrackingID());
+
+        return ResponseEntity.ok(trackOrderResponse);
+    }
+}
