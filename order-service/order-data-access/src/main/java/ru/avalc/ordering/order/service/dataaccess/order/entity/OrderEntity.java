9e24d2e40529ba7f5bf24cb45f230f0fde13ee85
Alexei Valchuk <a.valchukav@gmail.com>
2023-09-13 08:20:41 +0400

    refactor: delombok @Builder due to compilation error

--- order-service/order-data-access/src/main/java/ru/avalc/ordering/order/service/dataaccess/order/entity/OrderEntity.java
+++ order-service/order-data-access/src/main/java/ru/avalc/ordering/order/service/dataaccess/order/entity/OrderEntity.java
@@ -1,6 +1,9 @@
 package ru.avalc.ordering.order.service.dataaccess.order.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ru.avalc.ordering.system.domain.valueobject.OrderStatus;
 
 import javax.persistence.*;
@@ -17,7 +20,6 @@ import java.util.UUID;
 @Entity
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderEntity {
@@ -41,6 +43,10 @@ public class OrderEntity {
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItemEntity> items;
 
+    public static OrderEntityBuilder builder() {
+        return new OrderEntityBuilder();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -51,5 +57,74 @@ public class OrderEntity {
     @Override
     public int hashCode() {
         return Objects.hash(getId());
+    }
+
+    public static class OrderEntityBuilder {
+
+        private UUID id;
+        private UUID customerID;
+        private UUID restaurantID;
+        private UUID trackingID;
+        private BigDecimal price;
+        private OrderStatus orderStatus;
+        private String failureMessages;
+        private OrderAddressEntity orderAddress;
+        private List<OrderItemEntity> items;
+
+        OrderEntityBuilder() {
+        }
+
+        public OrderEntityBuilder id(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public OrderEntityBuilder customerID(UUID customerID) {
+            this.customerID = customerID;
+            return this;
+        }
+
+        public OrderEntityBuilder restaurantID(UUID restaurantID) {
+            this.restaurantID = restaurantID;
+            return this;
+        }
+
+        public OrderEntityBuilder trackingID(UUID trackingID) {
+            this.trackingID = trackingID;
+            return this;
+        }
+
+        public OrderEntityBuilder price(BigDecimal price) {
+            this.price = price;
+            return this;
+        }
+
+        public OrderEntityBuilder orderStatus(OrderStatus orderStatus) {
+            this.orderStatus = orderStatus;
+            return this;
+        }
+
+        public OrderEntityBuilder failureMessages(String failureMessages) {
+            this.failureMessages = failureMessages;
+            return this;
+        }
+
+        public OrderEntityBuilder orderAddress(OrderAddressEntity orderAddress) {
+            this.orderAddress = orderAddress;
+            return this;
+        }
+
+        public OrderEntityBuilder items(List<OrderItemEntity> items) {
+            this.items = items;
+            return this;
+        }
+
+        public OrderEntity build() {
+            return new OrderEntity(id, customerID, restaurantID, trackingID, price, orderStatus, failureMessages, orderAddress, items);
+        }
+
+        public String toString() {
+            return "OrderEntity.OrderEntityBuilder(id=" + this.id + ", customerID=" + this.customerID + ", restaurantID=" + this.restaurantID + ", trackingID=" + this.trackingID + ", price=" + this.price + ", orderStatus=" + this.orderStatus + ", failureMessages=" + this.failureMessages + ", orderAddress=" + this.orderAddress + ", items=" + this.items + ")";
+        }
     }
 }
