5879140b5b5ba957a2b3f134cf251e63969d87be
Alexei Valchuk <a.valchukav@gmail.com>
2023-09-13 13:40:52 +0400

    chore: add sql scripts for Customer service

---
+++ customer-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer.customers(id, username, first_name, last_name)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb41', 'user_1', 'First', 'User');
+
+INSERT INTO customer.customers(id, username, first_name, last_name)
+VALUES ('ea17dba4-51fa-11ee-be56-0242ac120002', 'user_2', 'Second', 'User');
