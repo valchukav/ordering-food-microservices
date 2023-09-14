f0e39dffe1ed140759515fec895d4aa10a7ef9c0
Alexei Valchuk <a.valchukav@gmail.com>
2023-09-14 11:03:13 +0400

    feat: provide validation for Payment service dto

--- payment-service/payment-domain/payment-application-dto/src/main/java/ru/avalc/ordering/payment/service/dto/PaymentRequest.java
+++ payment-service/payment-domain/payment-application-dto/src/main/java/ru/avalc/ordering/payment/service/dto/PaymentRequest.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import ru.avalc.ordering.system.domain.valueobject.PaymentOrderStatus;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.Instant;
 
@@ -17,8 +18,14 @@ public class PaymentRequest {
 
     private final String id;
     private final String sagaID;
+
+    @NotNull
     private final String orderID;
+
+    @NotNull
     private final String customerID;
+
+    @NotNull
     private final BigDecimal price;
     private final Instant createdAt;
     @Setter
