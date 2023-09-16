10cec06f0cebe2e121a37d801758c4a08804e2d5
Alexei Valchuk <a.valchukav@gmail.com>
2023-09-16 15:44:59 +0400

    chore: provide outbox scheduler for payment in Order service

---
+++ infrastructure/saga/src/main/java/ru/avalc/ordering/saga/order/SagaConstants.java
@@ -0,0 +1,13 @@
+package ru.avalc.ordering.saga.order;
+
+/**
+ * @author Alexei Valchuk, 16.09.2023, email: a.valchukav@gmail.com
+ */
+
+public final class SagaConstants {
+
+    public static final String ORDER_SAGA_NAME = "OrderProcessingSaga";
+
+    private SagaConstants() {
+    }
+}
---
+++ order-service/order-domain/order-application-service/src/main/java/ru/avalc/ordering/service/domain/outbox/scheduler/payment/PaymentOutboxHelper.java
@@ -0,0 +1,51 @@
+package ru.avalc.ordering.service.domain.outbox.scheduler.payment;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import ru.avalc.ordering.domain.exception.OrderDomainException;
+import ru.avalc.ordering.outbox.OutboxStatus;
+import ru.avalc.ordering.saga.SagaStatus;
+import ru.avalc.ordering.service.domain.outbox.model.payment.OrderPaymentOutboxMessage;
+import ru.avalc.ordering.service.domain.ports.output.repository.PaymentOutboxRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static ru.avalc.ordering.saga.order.SagaConstants.ORDER_SAGA_NAME;
+
+/**
+ * @author Alexei Valchuk, 16.09.2023, email: a.valchukav@gmail.com
+ */
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class PaymentOutboxHelper {
+
+    private final PaymentOutboxRepository paymentOutboxRepository;
+
+    @Transactional(readOnly = true)
+    public Optional<List<OrderPaymentOutboxMessage>> getPaymentOutboxMessage(OutboxStatus outboxStatus, SagaStatus... sagaStatuses) {
+        return paymentOutboxRepository.findByTypeAndOutboxStatusAndSagaStatus(ORDER_SAGA_NAME, outboxStatus, sagaStatuses);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<List<OrderPaymentOutboxMessage>> getPaymentOutboxMessage(UUID sagaID, SagaStatus... sagaStatuses) {
+        return paymentOutboxRepository.findByTypeAndSagaIDAndSagaStatus(ORDER_SAGA_NAME, sagaID, sagaStatuses);
+    }
+
+    @Transactional
+    public void save(OrderPaymentOutboxMessage orderPaymentOutboxMessage) {
+        OrderPaymentOutboxMessage response = paymentOutboxRepository.save(orderPaymentOutboxMessage);
+        if (response == null) {
+            String message = "Could not save OrderPaymentOutboxMessage with outbox id: " + orderPaymentOutboxMessage.getId();
+            log.error(message);
+            throw new OrderDomainException(message);
+        }
+
+        log.info("OrderPaymentOutboxMessage is saved with outbox id: {}", orderPaymentOutboxMessage.getId());
+    }
+}
