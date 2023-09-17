074e5c836cb8b17c12d5293b2dcc7c64c9e79644
Alexei Valchuk <a.valchukav@gmail.com>
2023-09-17 16:56:23 +0400

    refactor: provide common outbox scheduler cleaner abstract class

---
+++ order-service/order-domain/order-application-service/src/main/java/ru/avalc/ordering/service/domain/outbox/scheduler/OrderOutboxCleanerScheduler.java
@@ -0,0 +1,53 @@
+package ru.avalc.ordering.service.domain.outbox.scheduler;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import ru.avalc.ordering.outbox.OutboxScheduler;
+import ru.avalc.ordering.outbox.OutboxStatus;
+import ru.avalc.ordering.saga.SagaStatus;
+import ru.avalc.ordering.service.domain.outbox.model.OutboxMessage;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * @author Alexei Valchuk, 17.09.2023, email: a.valchukav@gmail.com
+ */
+
+@Slf4j
+@AllArgsConstructor
+public abstract class OrderOutboxCleanerScheduler<T extends OutboxMessage> implements OutboxScheduler {
+
+    private final OutboxHelper<T> outboxHelper;
+
+    @Override
+    @Scheduled(cron = "@midnight")
+    public void processOutboxMessage() {
+
+        Optional<List<T>> outboxMessagesResponse = outboxHelper.getOutboxMessage(
+                OutboxStatus.COMPLETED,
+                SagaStatus.SUCCEEDED,
+                SagaStatus.FAILED,
+                SagaStatus.COMPENSATED
+        );
+
+        if (outboxMessagesResponse.isPresent()) {
+            List<T> outboxMessages = outboxMessagesResponse.get();
+            log.info("Received {} {} for cleanup. The payloads: {}",
+                    outboxMessages.size(),
+                    outboxMessages.getClass().getName(),
+                    outboxMessages.stream().map(OutboxMessage::getPayload)
+                            .collect(Collectors.joining("\n")));
+
+            outboxHelper.deleteOutboxMessage(
+                    OutboxStatus.COMPLETED,
+                    SagaStatus.SUCCEEDED,
+                    SagaStatus.FAILED,
+                    SagaStatus.COMPENSATED);
+
+            log.info("{} {} deleted", outboxMessages.size(), outboxMessages.getClass().getName());
+        }
+    }
+}
--- order-service/order-domain/order-application-service/src/main/java/ru/avalc/ordering/service/domain/outbox/scheduler/payment/PaymentOutboxCleanerScheduler.java
+++ order-service/order-domain/order-application-service/src/main/java/ru/avalc/ordering/service/domain/outbox/scheduler/payment/PaymentOutboxCleanerScheduler.java
@@ -1,55 +1,17 @@
 package ru.avalc.ordering.service.domain.outbox.scheduler.payment;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import ru.avalc.ordering.outbox.OutboxScheduler;
-import ru.avalc.ordering.outbox.OutboxStatus;
-import ru.avalc.ordering.saga.SagaStatus;
-import ru.avalc.ordering.service.domain.outbox.model.OutboxMessage;
 import ru.avalc.ordering.service.domain.outbox.model.payment.OrderPaymentOutboxMessage;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import ru.avalc.ordering.service.domain.outbox.scheduler.OrderOutboxCleanerScheduler;
 
 /**
  * @author Alexei Valchuk, 17.09.2023, email: a.valchukav@gmail.com
  */
 
-@Slf4j
 @Component
-@AllArgsConstructor
-public class PaymentOutboxCleanerScheduler implements OutboxScheduler {
+public class PaymentOutboxCleanerScheduler extends OrderOutboxCleanerScheduler<OrderPaymentOutboxMessage> {
 
-    private final PaymentOutboxHelper paymentOutboxHelper;
-
-    @Override
-    @Scheduled(cron = "@midnight")
-    public void processOutboxMessage() {
-
-        Optional<List<OrderPaymentOutboxMessage>> paymentOutboxMessagesResponse = paymentOutboxHelper.getOutboxMessage(
-                OutboxStatus.COMPLETED,
-                SagaStatus.SUCCEEDED,
-                SagaStatus.FAILED,
-                SagaStatus.COMPENSATED
-        );
-
-        if (paymentOutboxMessagesResponse.isPresent()) {
-            List<OrderPaymentOutboxMessage> outboxMessages = paymentOutboxMessagesResponse.get();
-            log.info("Received {} OrderPaymentOutboxMessage for cleanup. The payloads: {}",
-                    outboxMessages.size(),
-                    outboxMessages.stream().map(OutboxMessage::getPayload)
-                            .collect(Collectors.joining("\n")));
-
-            paymentOutboxHelper.deleteOutboxMessage(
-                    OutboxStatus.COMPLETED,
-                    SagaStatus.SUCCEEDED,
-                    SagaStatus.FAILED,
-                    SagaStatus.COMPENSATED);
-
-            log.info("{} OrderPaymentOutboxMessage deleted", outboxMessages.size());
-        }
+    public PaymentOutboxCleanerScheduler(PaymentOutboxHelper outboxHelper) {
+        super(outboxHelper);
     }
 }
