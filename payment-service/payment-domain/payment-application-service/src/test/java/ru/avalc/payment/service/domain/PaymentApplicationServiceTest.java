be6218c2d854062fa6104143a3c3a8773be1c92f
Alexei Valchuk <a.valchukav@gmail.com>
2023-09-19 10:21:36 +0400

    test/refactor: update unit tests for Payment service due to outbox pattern

--- payment-service/payment-domain/payment-application-service/src/test/java/ru/avalc/payment/service/domain/PaymentApplicationServiceTest.java
+++ payment-service/payment-domain/payment-application-service/src/test/java/ru/avalc/payment/service/domain/PaymentApplicationServiceTest.java
@@ -1,31 +1,39 @@
 package ru.avalc.payment.service.domain;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.avalc.ordering.domain.exception.OrderDomainException;
+import ru.avalc.ordering.outbox.OutboxStatus;
+import ru.avalc.ordering.payment.service.domain.PaymentDomainServiceImpl;
 import ru.avalc.ordering.payment.service.domain.entity.CreditEntry;
 import ru.avalc.ordering.payment.service.domain.entity.CreditHistory;
 import ru.avalc.ordering.payment.service.domain.entity.Payment;
-import ru.avalc.ordering.payment.service.domain.event.PaymentCancelledEvent;
-import ru.avalc.ordering.payment.service.domain.event.PaymentCompletedEvent;
-import ru.avalc.ordering.payment.service.domain.event.PaymentEvent;
-import ru.avalc.ordering.payment.service.domain.event.PaymentFailedEvent;
+import ru.avalc.ordering.payment.service.domain.exception.PaymentNotFoundException;
 import ru.avalc.ordering.payment.service.domain.valueobject.CreditEntityID;
 import ru.avalc.ordering.payment.service.domain.valueobject.CreditHistoryID;
+import ru.avalc.ordering.payment.service.domain.valueobject.PaymentID;
 import ru.avalc.ordering.payment.service.domain.valueobject.TransactionType;
 import ru.avalc.ordering.payment.service.dto.PaymentRequest;
+import ru.avalc.ordering.saga.SagaStatus;
 import ru.avalc.ordering.system.domain.valueobject.CustomerID;
 import ru.avalc.ordering.system.domain.valueobject.Money;
 import ru.avalc.ordering.system.domain.valueobject.OrderID;
 import ru.avalc.ordering.system.domain.valueobject.PaymentStatus;
 import ru.avalc.ordering.tests.OrderingTest;
 import ru.avalc.payment.service.domain.exception.PaymentApplicationServiceException;
+import ru.avalc.payment.service.domain.outbox.model.OrderEventPayload;
+import ru.avalc.payment.service.domain.outbox.model.OrderOutboxMessage;
 import ru.avalc.payment.service.domain.ports.output.repository.CreditEntryRepository;
 import ru.avalc.payment.service.domain.ports.output.repository.CreditHistoryRepository;
+import ru.avalc.payment.service.domain.ports.output.repository.OrderOutboxRepository;
 import ru.avalc.payment.service.domain.ports.output.repository.PaymentRepository;
 
 import java.math.BigDecimal;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -33,8 +41,8 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static ru.avalc.ordering.saga.order.SagaConstants.ORDER_SAGA_NAME;
 
 /**
  * @author Alexei Valchuk, 13.09.2023, email: a.valchukav@gmail.com
@@ -55,6 +63,12 @@ public class PaymentApplicationServiceTest extends OrderingTest {
     @Autowired
     private PaymentRepository paymentRepository;
 
+    @Autowired
+    private OrderOutboxRepository orderOutboxRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     private final UUID creditEntryID = UUID.randomUUID();
     private final String paymentRequestID = UUID.randomUUID().toString();
 
@@ -62,12 +76,8 @@ public class PaymentApplicationServiceTest extends OrderingTest {
     private final BigDecimal creditHistory_2_amount = BigDecimal.valueOf(50);
 
     private PaymentRequest paymentRequest;
-    private PaymentRequest paymentRequestWithHugeAmount;
-    private PaymentRequest paymentRequestWithNegativeAmount;
 
     private Payment payment;
-    private Payment paymentWithHugeAmount;
-    private Payment negativePayment;
 
     private CreditEntry creditEntry;
     private BigDecimal totalCreditEntryAmount;
@@ -89,21 +99,10 @@ public class PaymentApplicationServiceTest extends OrderingTest {
                 .build();
 
         payment = Payment.builder()
+                .paymentID(new PaymentID(PAYMENT_ID))
                 .customerID(new CustomerID(CUSTOMER_ID))
                 .orderID(new OrderID(ORDER_ID))
                 .price(new Money(50))
-                .build();
-
-        negativePayment = Payment.builder()
-                .customerID(new CustomerID(CUSTOMER_ID))
-                .orderID(new OrderID(ORDER_ID))
-                .price(new Money(-50))
-                .build();
-
-        paymentWithHugeAmount = Payment.builder()
-                .customerID(new CustomerID(CUSTOMER_ID))
-                .orderID(new OrderID(ORDER_ID))
-                .price(new Money(170))
                 .build();
 
         totalCreditEntryAmount = creditHistory_1_amount.subtract(creditHistory_2_amount);
@@ -116,23 +115,10 @@ public class PaymentApplicationServiceTest extends OrderingTest {
 
         paymentRequest = PaymentRequest.builder()
                 .id(paymentRequestID)
+                .sagaID(SAGA_ID.toString())
                 .orderID(ORDER_ID.toString())
                 .customerID(CUSTOMER_ID.toString())
                 .price(payment.getPrice().getAmount())
-                .build();
-
-        paymentRequestWithHugeAmount = PaymentRequest.builder()
-                .id(paymentRequestID)
-                .orderID(ORDER_ID.toString())
-                .customerID(CUSTOMER_ID.toString())
-                .price(paymentWithHugeAmount.getPrice().getAmount())
-                .build();
-
-        paymentRequestWithNegativeAmount = PaymentRequest.builder()
-                .id(paymentRequestID)
-                .orderID(ORDER_ID.toString())
-                .customerID(CUSTOMER_ID.toString())
-                .price(negativePayment.getPrice().getAmount())
                 .build();
 
         when(paymentRepository.findByOrderId(any())).thenReturn(Optional.of(payment));
@@ -140,61 +126,44 @@ public class PaymentApplicationServiceTest extends OrderingTest {
         when(creditHistoryRepository.findByCustomerId(new CustomerID(CUSTOMER_ID)))
                 .thenReturn(Optional.of(new ArrayList<>(List.of(creditHistory_1, creditHistory_2))));
         when(paymentRepository.save(any(Payment.class))).thenReturn(payment);
+        when(orderOutboxRepository.save(any(OrderOutboxMessage.class))).thenReturn(getOrderOutboxMessage());
+    }
+
+    private OrderOutboxMessage getOrderOutboxMessage() {
+        OrderEventPayload orderEventPayload = OrderEventPayload.builder()
+                .paymentID(PAYMENT_ID.toString())
+                .orderID(ORDER_ID.toString())
+                .customerID(CUSTOMER_ID.toString())
+                .price(payment.getPrice().getAmount())
+                .createdAt(ZonedDateTime.now())
+                .paymentStatus(PaymentStatus.COMPLETED.name())
+                .build();
+
+        return OrderOutboxMessage.builder()
+                .id(UUID.randomUUID())
+                .sagaID(SAGA_ID)
+                .createdAt(ZonedDateTime.now())
+                .type(ORDER_SAGA_NAME)
+                .payload(createPayload(orderEventPayload))
+                .paymentStatus(PaymentStatus.COMPLETED)
+                .sagaStatus(SagaStatus.STARTED)
+                .outboxStatus(OutboxStatus.STARTED)
+                .version(0)
+                .build();
+    }
+
+    private String createPayload(OrderEventPayload orderEventPayload) {
+        try {
+            return objectMapper.writeValueAsString(orderEventPayload);
+        } catch (JsonProcessingException e) {
+            throw new OrderDomainException("Cannot create OrderPaymentEventPayload object!");
+        }
     }
 
     @Test
     public void completePayment() {
-        PaymentEvent paymentEvent = paymentRequestHelper.persistPayment(paymentRequest);
-
-        assertThat(paymentEvent.getPayment().getCustomerID()).isEqualTo(payment.getCustomerID());
-        assertThat(paymentEvent.getPayment().getPaymentStatus()).isEqualTo(PaymentStatus.COMPLETED);
-        assertThat(paymentEvent.getFailureMessages()).isEmpty();
+        paymentRequestHelper.persistPayment(paymentRequest);
         assertThat(creditEntry.getTotalCreditAmount().getAmount()).isEqualTo(totalCreditEntryAmount.subtract(payment.getPrice().getAmount()));
-        assertThat(paymentEvent).isInstanceOf(PaymentCompletedEvent.class);
-    }
-
-    @Test
-    public void completePaymentWithNegativePaymentAmount() {
-        when(paymentRepository.findByOrderId(any())).thenReturn(Optional.of(negativePayment));
-        when(paymentRepository.save(any(Payment.class))).thenReturn(negativePayment);
-
-        PaymentEvent paymentEvent = paymentRequestHelper.persistPayment(paymentRequestWithNegativeAmount);
-
-        assertThat(paymentEvent.getPayment().getCustomerID()).isEqualTo(payment.getCustomerID());
-        assertThat(paymentEvent.getPayment().getPaymentStatus()).isEqualTo(PaymentStatus.FAILED);
-        assertThat(paymentEvent.getFailureMessages()).isNotEmpty();
-        assertThat(paymentEvent).isInstanceOf(PaymentFailedEvent.class);
-    }
-
-    @Test
-    public void completePaymentWithAmountGreaterThanTotalCreditAmount() {
-        when(paymentRepository.findByOrderId(any())).thenReturn(Optional.of(paymentWithHugeAmount));
-        when(paymentRepository.save(any(Payment.class))).thenReturn(paymentWithHugeAmount);
-
-        PaymentEvent paymentEvent = paymentRequestHelper.persistPayment(paymentRequestWithHugeAmount);
-
-        assertThat(paymentEvent.getPayment().getCustomerID()).isEqualTo(paymentWithHugeAmount.getCustomerID());
-        assertThat(paymentEvent.getPayment().getPaymentStatus()).isEqualTo(PaymentStatus.FAILED);
-        assertThat(paymentEvent.getFailureMessages()).isNotEmpty();
-        assertThat(paymentEvent).isInstanceOf(PaymentFailedEvent.class);
-    }
-
-    @Test
-    public void completePaymentWithInvalidCreditEntry() {
-        CreditEntry creditEntry = CreditEntry.builder()
-                .creditEntityID(new CreditEntityID(creditEntryID))
-                .customerID(new CustomerID(CUSTOMER_ID))
-                .totalCreditAmount(new Money(2302))
-                .build();
-
-        when(creditEntryRepository.findByCustomerId(new CustomerID(CUSTOMER_ID))).thenReturn(Optional.of(creditEntry));
-
-        PaymentEvent paymentEvent = paymentRequestHelper.persistPayment(paymentRequest);
-
-        assertThat(paymentEvent.getPayment().getCustomerID()).isEqualTo(payment.getCustomerID());
-        assertThat(paymentEvent.getPayment().getPaymentStatus()).isEqualTo(PaymentStatus.FAILED);
-        assertThat(paymentEvent.getFailureMessages()).isNotEmpty();
-        assertThat(paymentEvent).isInstanceOf(PaymentFailedEvent.class);
     }
 
     @Test
@@ -212,27 +181,21 @@ public class PaymentApplicationServiceTest extends OrderingTest {
     }
 
     @Test
-    public void cancelPayment() {
-        PaymentEvent paymentEvent = paymentRequestHelper.persistCancelledPayment(paymentRequest);
+    public void completePaymentWhenMessageWithSagaIDIsAlreadyExists() {
+        when(orderOutboxRepository.findByTypeAndSagaIdAndPaymentStatusAndOutboxStatus(ORDER_SAGA_NAME, SAGA_ID,
+                PaymentStatus.COMPLETED, OutboxStatus.COMPLETED)).thenReturn(Optional.empty());
 
-        assertThat(paymentEvent.getPayment().getCustomerID()).isEqualTo(payment.getCustomerID());
-        assertThat(paymentEvent.getPayment().getPaymentStatus()).isEqualTo(PaymentStatus.CANCELLED);
-        assertThat(paymentEvent.getFailureMessages()).isEmpty();
-        assertThat(creditEntry.getTotalCreditAmount().getAmount()).isEqualTo(totalCreditEntryAmount.add(payment.getPrice().getAmount()));
-        assertThat(paymentEvent).isInstanceOf(PaymentCancelledEvent.class);
+        paymentRequestHelper.persistPayment(paymentRequest);
+
+        verify(mock(PaymentDomainServiceImpl.class), times(0))
+                .validateAndInitiatePayment(any(Payment.class), any(CreditEntry.class), anyList(), anyList());
     }
 
     @Test
-    public void cancelPaymentWithNegativePaymentAmount() {
-        when(paymentRepository.findByOrderId(any())).thenReturn(Optional.of(negativePayment));
-        when(paymentRepository.save(any(Payment.class))).thenReturn(negativePayment);
+    public void cancelPayment() {
+        paymentRequestHelper.persistCancelledPayment(paymentRequest);
 
-        PaymentEvent paymentEvent = paymentRequestHelper.persistCancelledPayment(paymentRequestWithNegativeAmount);
-
-        assertThat(paymentEvent.getPayment().getCustomerID()).isEqualTo(payment.getCustomerID());
-        assertThat(paymentEvent.getPayment().getPaymentStatus()).isEqualTo(PaymentStatus.FAILED);
-        assertThat(paymentEvent.getFailureMessages()).isNotEmpty();
-        assertThat(paymentEvent).isInstanceOf(PaymentFailedEvent.class);
+        assertThat(creditEntry.getTotalCreditAmount().getAmount()).isEqualTo(totalCreditEntryAmount.add(payment.getPrice().getAmount()));
     }
 
     @Test
@@ -247,5 +210,24 @@ public class PaymentApplicationServiceTest extends OrderingTest {
         when(creditHistoryRepository.findByCustomerId(new CustomerID(CUSTOMER_ID))).thenReturn(Optional.empty());
 
         assertThrows(PaymentApplicationServiceException.class, () -> paymentRequestHelper.persistCancelledPayment(paymentRequest));
+    }
+
+
+    @Test
+    public void cancelPaymentWithNoPayment() {
+        when(paymentRepository.findByOrderId(new OrderID(ORDER_ID))).thenReturn(Optional.empty());
+
+        assertThrows(PaymentNotFoundException.class, () -> paymentRequestHelper.persistCancelledPayment(paymentRequest));
+    }
+
+    @Test
+    public void cancelPaymentWhenMessageWithSagaIDIsAlreadyExists() {
+        when(orderOutboxRepository.findByTypeAndSagaIdAndPaymentStatusAndOutboxStatus(ORDER_SAGA_NAME, SAGA_ID,
+                PaymentStatus.COMPLETED, OutboxStatus.COMPLETED)).thenReturn(Optional.empty());
+
+        paymentRequestHelper.persistCancelledPayment(paymentRequest);
+
+        verify(mock(PaymentDomainServiceImpl.class), times(0))
+                .validateAndInitiatePayment(any(Payment.class), any(CreditEntry.class), anyList(), anyList());
     }
 }
