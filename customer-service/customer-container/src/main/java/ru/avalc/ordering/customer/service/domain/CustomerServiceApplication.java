eb534e73360ecb971bdb9feccc4ddd9e7d219228
Alexei Valchuk <a.valchukav@gmail.com>
2023-09-21 11:58:33 +0400

    chore: provide container module for Customer service

--- customer-service/customer-container/src/main/java/ru/avalc/ordering/customer/service/domain/CustomerServiceApplication.java
+++ customer-service/customer-container/src/main/java/ru/avalc/ordering/customer/service/domain/CustomerServiceApplication.java
@@ -1,12 +1,16 @@
-package ru.avalc.ordering.customer.service;
+package ru.avalc.ordering.customer.service.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author Alexei Valchuk, 13.09.2023, email: a.valchukav@gmail.com
  */
 
+@EnableJpaRepositories(basePackages = {"ru.avalc.ordering.customer.service.dataaccess", "ru.avalc.ordering.dataaccess"})
+@EntityScan(basePackages = {"ru.avalc.ordering.customer.service.dataaccess", "ru.avalc.ordering.dataaccess"})
 @SpringBootApplication(scanBasePackages = "ru.avalc.ordering")
 public class CustomerServiceApplication {
 
